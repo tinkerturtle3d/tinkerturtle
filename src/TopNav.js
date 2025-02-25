1a48d9538716d37d0378da60d23de5f92cf3103e
Alec Lindberg <aleclindberg@Alecs-MacBook-Pro-2024.local>
2025-02-24 23:17:23 -0800

    update topnav

--- src/TopNav.js
+++ src/TopNav.js
@@ -14,6 +14,7 @@ import { Link, useLocation } from 'react-router-dom';
 // };
 
 const getWindowDimensions = () =>{
+  const hasWindow = typeof window !== 'undefined';
   const width = hasWindow ? window.innerWidth : null;
   const height = hasWindow ? window.innerHeight : null;
   return {
